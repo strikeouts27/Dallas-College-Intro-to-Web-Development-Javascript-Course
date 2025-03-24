dc51c05111bbaddef19e43c338bc00804ed70324
strikeouts27 <stribling.andrew@gmail.com>
2025-03-23 22:05:22 -0500

    adding games.js file

---
+++ games.js
@@ -0,0 +1,28 @@
+const playBtn = document.querySelector(".playgame");
+
+function playGame() {
+  // Prompt the user for the answer
+  const userAnswerOne = prompt("Who was the creator of JavaScript?");
+  if (userAnswerOne.toUpperCase() === "BRENDAN EICH") {
+    document.getElementById("prompt").innerHTML =
+      "Yes that is correct! Brendan Eich created JavaScript in 1995! Let us take a moment and be thankful for his work! Here is a picture of Brendan, the Father of Javascript!";
+    presentProgrammer("/images/BRENDANEICH.jpg");
+  } else {
+    document.getElementById("prompt").innerHTML =
+      "Incorrect. The correct answer is Brendan Eich.";
+    presentProgrammer("/images/anotherProgrammer.jpg");
+  }
+  
+}
+
+function presentProgrammer(imageSrc) {
+  const imgElement = document.getElementById("programmerImage");
+  imgElement.src = imageSrc;
+  imgElement.style.display = "block";
+}
+
+function main() {
+  playBtn.addEventListener("click", playGame);
+}
+
+main();
