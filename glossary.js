1646e17842059801bf24e9e75c29f2752cd6e9cf
strikeouts27 <stribling.andrew@gmail.com>
2025-04-07 17:44:58 -0500

    added flower event handler

--- glossary.js
+++ glossary.js
@@ -1,15 +1,21 @@
-$(document).ready(function() {
-  console.log('Ready!');
+$(document).ready(function () {
+  // Hide all elements with the class 'botanic' on page load
+  $(".botanic").hide();
+
   // jQuery function calls go here...
-  $('h1, h2').hover(
-  function() {
-    $(this).css("color", "green"); 
-  },
-  function() {
-    // Mouseout function: Revert background color to white
-    $(this).css("color", "black"); 
+  $("h1, h2").hover(
+    function () {
+      $(this).css("color", "green");
+    },
+    function () {
+      // Mouseout function: Revert background color to white
+      $(this).css("color", "black");
+    }
+  );
+
+  // Add a click event to the flower class
+  $(".flower").click(function () {
+    $(".botanic").hide(); // Hide all botanic names
+    $(this).children(".botanic").show(); // Show only the botanic name for the clicked flower
   });
-
 });
-
-
