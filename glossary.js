73b4da51d5d10340584754921815bfba151f98a2
strikeouts27 <stribling.andrew@gmail.com>
2025-04-07 01:59:52 -0500

    mouseover mouseout

--- glossary.js
+++ glossary.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
+  console.log("Ready!");
+
+  // Mouseover event to change color to blue
   $("h1, h2").mouseover(function () {
-    $(this).css("color", "blue"); // Change the text color to blue
+    $(this).css("color", "blue");
   });
 
+  // Mouseout event to change color back to black
   $("h1, h2").mouseout(function () {
-    $(this).css("color", "black"); // Revert the text color to black
+    $(this).css("color", "black");
   });
 });
