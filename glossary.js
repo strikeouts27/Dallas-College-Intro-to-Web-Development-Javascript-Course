c68a46daecfc0ccc4509b0dedbdc0ca5f955d22e
strikeouts27 <stribling.andrew@gmail.com>
2025-04-13 13:47:06 -0500

    jQuery Effects and Animation course materials

--- glossary.js
+++ glossary.js
@@ -18,17 +18,27 @@ $(document).ready(function () {
   // Add hover event for the .pic class
   $(".pic").hover(
     function (evt) {
-      // Show the associated image and position it based on the cursor
-      const imgDiv = $(this).siblings(".imgdiv");
-      imgDiv.css({
-        top: evt.pageY + 10 + "px", // Position 10px below the cursor
-        left: evt.pageX + 10 + "px", // Position 10px to the right of the cursor
-        display: "block", // Make the image visible
-      });
+      // Get the title attribute and create the id for the .imgdiv
+      const imgId = "#" + $(this).attr("title");
+
+      // Get the X and Y coordinates of the event
+      const x = evt.pageX + 150; // Add 150 to move it to the right
+      const y = evt.pageY;
+
+      // Set the top and left CSS properties of the associated .imgdiv
+      $(imgId)
+        .css({
+          top: y + "px", // Set the top position
+          left: x + "px", // Set the left position
+        })
+        .show(); // Show the image
     },
     function () {
-      // Hide the image when the cursor leaves the .pic span
-      $(this).siblings(".imgdiv").css("display", "none");
+      // Get the title attribute and create the id for the .imgdiv
+      const imgId = "#" + $(this).attr("title");
+
+      // Hide the associated image div when the mouse leaves
+      $(imgId).hide();
     }
   );
 });
