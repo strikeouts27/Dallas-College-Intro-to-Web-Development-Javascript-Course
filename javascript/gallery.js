50e1c2a4390d9b007d0e499f84533038adffa6fe
strikeouts27 <stribling.andrew@gmail.com>
2025-04-28 15:55:53 -0500

    hover function works at this point

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,23 +1,20 @@
 $(document).ready(function () {
-  let originalSrc = $("#photo").attr("src");
-
-  /* Apply hover effect to smaller images */
-  $(".thumbnail").hover(
-    function (event) {
-      // Mouse enters the smaller image
-      let newSrc = $(event.currentTarget).attr("src");
-      $("#photo").attr("src", newSrc);
-    },
+  /* Using hover() function */
+  $('#thumbs img').hover(
     function () {
-      // Mouse leaves the smaller image
-      $("#photo").attr("src", originalSrc);
+      $(this).css({
+        'border': '2px solid darkgreen',
+        'box-shadow': '0 0 10px rgba(0, 100, 0, 0.7)'
+      });
+    }, 
+    function () {
+      $(this).css({
+        'border': '',
+        'box-shadow': ''
+      }); 
     }
   );
 
-  /* Update the large picture on click */
-  $(".thumbnail").on("click", function (event) {
-    // Update the originalSrc to the clicked image's src
-    originalSrc = $(event.currentTarget).attr("src");
-    $("#photo").attr("src", originalSrc);
-  });
+
+  
 });
