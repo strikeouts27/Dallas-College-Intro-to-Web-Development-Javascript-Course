afe31ef9d6a6ff10829d3b12c35ad0a2429ab199
strikeouts27 <stribling.andrew@gmail.com>
2025-05-16 01:57:43 -0500

    implemented javascript and jQuery methods

--- javascript/aboutus.js
+++ javascript/aboutus.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
   // Your jQuery code here
-});
+
+  $(".jQueryToggleButton").on("click", function () {
+    $(".jqueryLogo1").fadeToggle();
+  });
+
+  $(".SlideUpButton").on("click", function () {
+    $(".jQueryLogo2").slideUp();
+  })
+
+  $(".SlideDownButton").on("click", function () {
+    $(".jQueryLogo2").slideDown();
+  }
+);
+
+  // Converts US dollars to British pounds
+  function dollarsToPounds(dollars) {
+    const exchangeRate = 0.79; // Example rate, update as needed
+    return dollars * exchangeRate;
+  }
+
+  // Example usage:
+  const pounds = dollarsToPounds(100);
+  console.log(`100 USD is ${pounds} GBP`);
+}); 
